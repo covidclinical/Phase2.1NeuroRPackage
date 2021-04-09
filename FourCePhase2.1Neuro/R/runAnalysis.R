6950ae461dcfd447f6f73e31d1ccbe056f7132f2
lelaboratoire <grixor@gmail.com>
2021-04-08 21:25:10 -0400

    fix 'can only subtract from "Date" objects'

--- FourCePhase2.1Neuro/R/runAnalysis.R
+++ FourCePhase2.1Neuro/R/runAnalysis.R
@@ -72,9 +72,7 @@ runAnalysis <- function() {
         ~ lubridate::parse_date_time(.x, orders = c("mdy", "ymd"))
       ),
       last_discharge_date = pmin(death_date, last_discharge_date, na.rm = TRUE),
-      total_stay = as.numeric(last_discharge_date - admission_date,
-        units = "days"
-      )
+      total_stay = interval(admission_date, last_discharge_date) %/% days(1)
     )
 
   obs_raw <- obs_raw %>%
