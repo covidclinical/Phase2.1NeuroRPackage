d263f3cb826d54212d6e576342a48562238d6f12
Meghan Hutch <42354910+meghutch@users.noreply.github.com>
2022-11-08 10:59:43 -0600

    update script for debugging PAVIA mapped_comorbidity_table error

--- FourCePhase2.1Neuro/R/run.R
+++ FourCePhase2.1Neuro/R/run.R
@@ -943,6 +943,9 @@ get_elix_mat <- function(obs_raw, icd_version, t1 = -365, t2 = -15, map_type = "
 
   mapped_codes_table <- comorb_elix$mapped_codes_table
 
+  print('evaluate whether mapped_codes_table is null')
+  is.null(mapped_codes_table)
+
   comorb_list <- list(index_scores_elix = index_scores_elix,
                       mapped_codes_table = mapped_codes_table)
 
@@ -999,6 +1002,9 @@ process_comorb_data <- function(df, demo_raw, nstay_df, neuro_patients, icd_vers
     nstay_filtered <- nstay_df %>%
       filter(age_group %in% c("00to02", "06to11", "12to17"))
   }
+
+  print('evaluate whether mapped_codes_table is null')
+  is.null(mapped_codes_table)
 
   print('save mapped_codes_table')
   mapped_codes_table <- comorb_list$mapped_codes_table
@@ -1094,12 +1100,15 @@ process_comorb_data <- function(df, demo_raw, nstay_df, neuro_patients, icd_vers
 
     ## obfuscate comorbidity table
     print('construct mapped comorbidity codes table')
+    if(!is.null(mapped_codes_table)) {
     mapped_codes_table_obfus <- blur_it(mapped_codes_table, vars = 'n_patients', blur_abs, mask_thres)
 
     # remove categories with 0 patients
     mapped_codes_table_obfus <- mapped_codes_table_obfus %>%
       filter(!n_patients == 0)
-  }
+    } else {
+    mapped_codes_table_obfus = NULL
+    }
 
   if(is.na(elix_pca[1])) {
     print('no comorbidities present, creating empty dataframe')
